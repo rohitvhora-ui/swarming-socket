f9bf8991874189bd114c4c16d70096fbd2084d55
shobhit.c.jain <shobhit.c.jain@accenture.com>
2020-09-02 19:35:43 +0530

    fix to make client alive while client is connected.

--- app.js
+++ app.js
@@ -43,11 +43,22 @@ const emitOnlineUsers = (room) => {
    io.in(room).emit("usersOnline", getOnlineUsers(room));
 }
 
+const getApiAndEmit = socket => {
+   const response = new Date();
+   // Emitting a new message. Will be consumed by the client
+   socket.emit("FromAPI", response);
+ };
+
 io.on('connection', (socket) => {
    console.log(`Connected: ${socket.id}`);
+   if (connectionInterval) {
+      clearInterval(connectionInterval);
+    }
+    var connectionInterval = setInterval(() => getApiAndEmit(socket), 1000);
 
    socket.on('disconnect', () => {
       console.log(`Disconnected: ${socket.id}`)
+      clearInterval(connectionInterval);
       const { room } = socket;
       if (room) {
          emitOnlineUsers(socket.room);
